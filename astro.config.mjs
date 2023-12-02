aa77fe7cf22272446c99defe32c6f0185e21689b
HyūgoDev <h.mejias.ugalde53@gmail.com>
2023-12-01 23:33:13 -0600

    se agrega vercer como server

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import netlify from "@astrojs/netlify/functions";
 
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: netlify(),
+  adapter: vercel(),
   prefetch: true
 });
